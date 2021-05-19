c97027598b5d3178cb6b11bfa551c299423c6380
carneyaj <50503055+carneyaj@users.noreply.github.com>
2021-05-19 10:49:54 -0400

    Update params.py

--- params.py
+++ params.py
@@ -11,7 +11,7 @@ seconds = 30		# length of each window of analysis
 blocks = 1		# number of analysis blocks of length *seconds* 
 gain = 20   		# multiplier on audio samples
 
-save_directory = ""
+save_directory = "~/data"
 out_dtype = "float32"
 
 top_classes = 10
