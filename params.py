d3327e1668a38f7273c807ba29332ef154ed19a4
carneyaj <50503055+carneyaj@users.noreply.github.com>
2021-05-19 12:59:20 -0400

    Update params.py

--- params.py
+++ params.py
@@ -7,8 +7,8 @@ Resample to 16kHz
 '''
 
 samplerate = 16000	# Audio samplerate
-seconds = 30		# length of each window of analysis
-blocks = 1		# number of analysis blocks of length *seconds* 
+seconds = 20		# length of each window of analysis
+blocks = 2		# number of analysis blocks of length *seconds* 
 gain = 20   		# multiplier on audio samples
 
 save_directory = "/home/pi/data/"
