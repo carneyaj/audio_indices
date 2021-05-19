d14a5fd9c2c48b420b587c04b9c4d4ec9c7a6702
carneyaj <50503055+carneyaj@users.noreply.github.com>
2021-05-19 09:59:22 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d_%H%M")
 echo $DATE
-git -C /home/pi/audio_indices pull
+git -C /home/pi/audio_indices pull -v
 cp -r /home/pi/audio_indices/audioschedule /etc/cron.d
